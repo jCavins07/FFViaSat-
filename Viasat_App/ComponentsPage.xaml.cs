d2082f293e0572cdf2fb16626a191b8be7a54648
enrique95ae <enrique95ae@gmail.com>
2019-02-15 18:49:07 -0800

    Added ComponentsPage

---
+++ Viasat_App/ComponentsPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Viasat_App
+{
+    public partial class ComponentsPage : ContentPage
+    {
+        public ComponentsPage()
+        {
+            InitializeComponent();
+        }
+    }
+}

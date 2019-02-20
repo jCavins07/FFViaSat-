fd1513f5f57ef42c3293804944bd0da5de4cd569
enrique95ae <enrique95ae@gmail.com>
2019-02-19 17:55:47 -0800

    ResultsPage + ListViewItemTapped

---
+++ Viasat_App/ItemModel.cs
@@ -0,0 +1,11 @@
+﻿using Xamarin.Forms;
+
+namespace Viasat_App
+{
+    public class ItemModel
+    {
+        public string ItemTitle { get; set; }
+        public string ItemSerial { get; set; }
+        public string ItemDescription { get; set; }
+    }
+}
--- Viasat_App/ResultsPage.xaml.cs
+++ Viasat_App/ResultsPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 
 using Xamarin.Forms;
 
@@ -7,18 +8,59 @@ namespace Viasat_App
 {
     public partial class ResultsPage : ContentPage
     {
+
+        public ObservableCollection<ItemModel> ItemList { get; set; }
+
+
         public ResultsPage()
         {
             InitializeComponent();
+            PopulateList();
         }
 
         //START: BUTTONS EVENTS #######################################################
 
-        private async void itemButton_Clicked(object sender, EventArgs e)
+        private async void itemEntry_Tapped(object sender, EventArgs e)
         {
             await Navigation.PushAsync(new ItemPage());
         }
-        
-        //END: BUTTONS EVENTS #######################################################
+
+        //END: BUTTONS EVENTS #########################################################
+
+
+
+
+
+        //START: DEMONSTRATION PURPOSES  ##############################################
+
+        private void PopulateList()
+        {
+            ItemList = new ObservableCollection<ItemModel>()
+            {
+                new ItemModel()
+                {
+                    ItemTitle = "Title1",
+                    ItemDescription = "Description1",
+                    ItemSerial = "1234564-565y24531-243561"
+                },
+
+                new ItemModel()
+                {
+                    ItemTitle = "Title2",
+                    ItemDescription = "Description2",
+                    ItemSerial = "123412-ACVBNGTS-F222"
+                },
+
+                new ItemModel()
+                {
+                    ItemTitle = "Title3",
+                    ItemDescription = "Description3",
+                    ItemSerial = "1234123-453-212-12222222"
+                }
+            };
+
+            ResultsListView.ItemsSource = ItemList;
+        }
+
     }
 }

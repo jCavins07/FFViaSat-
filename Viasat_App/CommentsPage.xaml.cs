8d13906c29b3e28a18ab0387ddb4b5f0b83b2340
enrique95ae <enrique95ae@gmail.com>
2019-02-20 17:45:12 -0800

    Add commentsPage and commentPage

--- Viasat_App/CommentsPage.xaml.cs
+++ Viasat_App/CommentsPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 
 using Xamarin.Forms;
 
@@ -7,9 +8,53 @@ namespace Viasat_App
 {
     public partial class CommentsPage : ContentPage
     {
+
+        public ObservableCollection<CommentModel> CommentList { set; get; }
+
         public CommentsPage()
         {
             InitializeComponent();
+            populateList();
+        }
+
+
+
+        private async void commentEntry_Tapped(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new CommentPage());
+        }
+
+
+        private void populateList()
+        {
+            CommentList = new ObservableCollection<CommentModel>()
+            {
+                new CommentModel()
+                {
+                    CommentTitle = "comment1title",
+                    CommentDescription = "comment1description",
+                    CommentDate = "02/19/2019",
+                    CommentAuthor = "Author1"
+                },
+
+                new CommentModel()
+                {
+                    CommentTitle = "comment2title",
+                    CommentDescription = "comment2description",
+                    CommentDate = "02/19/2019",
+                    CommentAuthor = "Author2"
+                },
+
+                new CommentModel()
+                {
+                    CommentTitle = "comment3title",
+                    CommentDescription = "comment3description",
+                    CommentDate = "02/19/2019",
+                    CommentAuthor = "Author3"
+
+                },
+            };
+            CommentsListView.ItemsSource = CommentList;
         }
     }
 }
---
+++ Viasat_App/Models/CommentModel.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace Viasat_App
+{
+    public class CommentModel
+    {
+        public string CommentTitle { get; set; }
+        public string CommentDescription { get; set; }
+        public string CommentDate { get; set; }
+        public string CommentAuthor { get; set; }
+    }
+}

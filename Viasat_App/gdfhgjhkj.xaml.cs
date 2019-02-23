d8ed74fdeed45f6a8a65dcc9c95a14b8a547a93e
enrique95ae <enrique95ae@gmail.com>
2019-02-22 16:39:37 -0800

    test

---
+++ Viasat_App/gdfhgjhkj.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Viasat_App
+{
+    public partial class gdfhgjhkj : ContentPage
+    {
+        public gdfhgjhkj()
+        {
+            InitializeComponent();
+        }
+    }
+}

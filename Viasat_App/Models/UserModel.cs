70df5939edd85cd47c54e43205ebcca474e092c6
enrique95ae <enrique95ae@gmail.com>
2019-02-25 16:47:07 -0800

    UserModel and Globals

---
+++ Viasat_App/Models/UserModel.cs
@@ -0,0 +1,10 @@
+﻿using Xamarin.Forms;
+
+namespace Viasat_App
+{
+    public class UserModel
+    {
+        public string UserName { get; set; }
+        public string UserAccessLevel { get; set; }
+    }
+}
